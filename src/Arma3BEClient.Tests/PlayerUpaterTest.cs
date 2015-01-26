1d0e6d229d071c94d74d07809a8af186c59ce30c
Artem Muradov <tym32167@gmail.com>
2015-01-26 20:21:28 +0500

    uncommented tests

--- src/Arma3BEClient.Tests/PlayerUpaterTest.cs
+++ src/Arma3BEClient.Tests/PlayerUpaterTest.cs
@@ -1,16 +1,15 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace Arma3BEClient.Tests
-//{
-//    [TestClass]
-//    public class PlayerUpaterTest
-//    {
-//        [TestMethod]
-//        public void Parse1()
-//        {
-//            var p =Updater.Models.Player.Parse("17  188.162.228.192:63255 172  d074b6194fa864e50956a8294077f24a(OK) Cpt. JET");
-//            Assert.IsNotNull(p);
-//        }
-//    } 
-//}
+namespace Arma3BEClient.Tests
+{
+    [TestClass]
+    public class PlayerUpaterTest
+    {
+        [TestMethod]
+        public void Parse1()
+        {
+            var p = Updater.Models.Player.Parse("17  188.162.228.192:63255 172  d074b6194fa864e50956a8294077f24a(OK) Cpt. JET");
+            Assert.IsNotNull(p);
+        }
+    }
+}
