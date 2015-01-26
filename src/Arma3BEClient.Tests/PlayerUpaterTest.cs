5e928dc9c5c187defb3811100a1ba7fe97733f1c
Artem Muradov <tym32167@gmail.com>
2015-01-26 18:18:14 +0500

    #01-1

--- src/Arma3BEClient.Tests/PlayerUpaterTest.cs
+++ src/Arma3BEClient.Tests/PlayerUpaterTest.cs
@@ -12,5 +12,5 @@
 //            var p =Updater.Models.Player.Parse("17  188.162.228.192:63255 172  d074b6194fa864e50956a8294077f24a(OK) Cpt. JET");
 //            Assert.IsNotNull(p);
 //        }
-//    }
+//    } 
 //}
