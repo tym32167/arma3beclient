7348328b1c06c5f4c801a480453301102e494a8a
Artem Muradov <tym32167@gmail.com>
2015-07-04 23:41:59 +0500

    #A3BET-6 Раделил проекты, убрал ненужные зависимости

--- src/Arma3BEClient.Tests/PlayerUpaterTest.cs
+++ src/Arma3BEClient.Tests/PlayerUpaterTest.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace Arma3BEClient.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class PlayerUpaterTest
     {
-        [TestMethod]
+        [Test]
         public void Parse1()
         {
             var p = Updater.Models.Player.Parse("17  188.162.228.192:63255 172  d074b6194fa864e50956a8294077f24a(OK) Cpt. JET");
