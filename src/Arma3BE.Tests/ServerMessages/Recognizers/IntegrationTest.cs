18e5f39b79b4085d24b8669e84bba7a27144672e
Artem Muradov <tym32167@gmail.com>
2015-10-11 22:12:26 +0500

    Начал добавлять тесты

---
+++ src/Arma3BE.Tests/ServerMessages/Recognizers/IntegrationTest.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+namespace Arma3BEClient.Tests.ServerMessages.Recognizers
+{
+    [TestFixture]
+    [Ignore]
+    public class IntegrationTest
+    {
+         
+    }
+}
