0536cd4ad8e9a74fb0bb0dc7ce6505089831b0cc
Artem Muradov <tym32167@gmail.com>
2015-10-18 14:33:35 +0500

    Выборка сообщений о списке активных пользователей + тесты

--- src/Arma3BE.Server/Messaging/Recognizers/PlayerListRecognizer.cs
+++ src/Arma3BE.Server/Messaging/Recognizers/PlayerListRecognizer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text.RegularExpressions;
 using Arma3BE.Server.Models;
 
 namespace Arma3BE.Server.Messaging.Recognizers
@@ -11,7 +13,60 @@ namespace Arma3BE.Server.Messaging.Recognizers
 
         public bool CanRecognize(ServerMessage serverMessage)
         {
-            return serverMessage.Message.Contains("Players on server:");
+            var firstLines = new[]
+            {
+                "Players on server:",
+                "[#] [IP Address]:[Port] [Ping] [GUID] [Name]",
+                "--------------------------------------------------"
+            };
+
+
+            var lines = serverMessage.Message.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            if (lines.Length <= firstLines.Length) return false;
+
+            var i = 0;
+            for (; i < firstLines.Length; i++)
+            {
+                if (string.Compare(firstLines[i], lines[i], StringComparison.InvariantCultureIgnoreCase) != 0)
+                    return false;
+            }
+
+            for (; i < (lines.Length - 1); i++)
+            {
+                if (Player.Parse(lines[i]) == null || !CanRecognizePlayerLine(lines[i]))
+                {
+                    return false;
+                }
+            }
+
+
+            var lastLine = $"({lines.Length - 4} players in total)";
+            if (string.Compare(lastLine, lines[lines.Length - 1], StringComparison.InvariantCultureIgnoreCase) != 0)
+                return false;
+
+
+            return true;
         }
+
+        public bool CanRecognizePlayerLine(string line)
+        {
+            var lines = line.Split(" \t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length < 5) return false;
+
+            int test;
+            if (!Int32.TryParse(lines[0], out test)) return false;
+
+            if (!RegexIpAndPort.IsMatch(lines[1])) return false;
+            if (lines[1].Split(".:".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Length != 5) return false;
+
+            if (!Int32.TryParse(lines[2], out test)) return false;
+
+            return true;
+        }
+
+        private static readonly Regex RegexIpAndPort = new Regex(
+            @"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):([\d]+)",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
     }
 }
---
+++ src/Arma3BE.Tests/ServerMessages/Recognizers/PlayerListLineRecognizerTest.cs
@@ -0,0 +1,32 @@
+﻿using Arma3BE.Server.Messaging.Recognizers;
+using NUnit.Framework;
+
+namespace Arma3BEClient.Tests.ServerMessages.Recognizers
+{
+    [TestFixture]
+    public class PlayerListLineRecognizerTest
+    {
+        [Test]
+        [TestCase("0   5.141.88.80:59558     31   someGUIDnumbers(OK) hobot")]
+        [TestCase("5   62.122.208.170:2304   16   someGUIDnumbers(OK) Ranger  (Lobby)")]
+        [TestCase("10  188.244.37.117:2304   15   someGUIDnumbers(OK) ����177")]
+        [TestCase("14  178.34.203.78:18944   47   someGUIDnumbers(OK) Aleksandres (Lobby)")]
+        [TestCase("31  46.231.212.34:2304    0    someGUIDnumbers(OK) [GH] Listopad")]
+        [TestCase("31  46.231.212.34:2304    0    someGUIDnumbers(OK) [GH] Listopad (Lobby)")]
+        public void Line_Correct_Test(string line)
+        {
+            var recognizer = new PlayerListRecognizer();
+            Assert.IsTrue(recognizer.CanRecognizePlayerLine(line));
+        }
+
+        [Test]
+        [TestCase("0s   5.141.88.80:59558     31   someGUIDnumbers(OK) hobot")]
+        [TestCase("5   62..12.2.208.170:2304   16   someGUIDnumbers(OK) Ranger  (Lobby)")]
+        [TestCase("10  188.244.37.117:2304   1d5   someGUIDnumbers(OK) ����177")]
+        public void Line_Not_Correct_Test(string line)
+        {
+            var recognizer = new PlayerListRecognizer();
+            Assert.IsFalse(recognizer.CanRecognizePlayerLine(line));
+        }
+    }
+}
