e47544ea1d399368684ce19fd6fe913d37f23359
Artem Muradov <tym32167@gmail.com>
2016-04-20 21:58:09 +0200

    shell window

---
+++ src/Arma3BE.Frontend/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Arma3BE.Frontend
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var boostrapper = new Bootstrapper();
+            boostrapper.Run();
+        }
+    }
+}
---
+++ src/Arma3BE.Frontend/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace Arma3BE.Frontend
+{
+    internal class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
