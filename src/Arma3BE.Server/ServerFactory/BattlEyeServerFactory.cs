63056bd4fb1474077444e855edd43f2e2b45db05
Artem Muradov <tym32167@gmail.com>
2016-07-12 23:17:36 +0200

    disable fake server

--- src/Arma3BE.Server/ServerFactory/BattlEyeServerFactory.cs
+++ src/Arma3BE.Server/ServerFactory/BattlEyeServerFactory.cs
@@ -1,5 +1,6 @@
 using Arma3BE.Server.Abstract;
 using Arma3BE.Server.Mocks;
+using Arma3BE.Server.ServerDecorators;
 using Arma3BEClient.Common.Logging;
 using BattleNET;
 
@@ -16,12 +17,12 @@ namespace Arma3BE.Server.ServerFactory
 
         public IBattlEyeServer Create(BattlEyeLoginCredentials credentials)
         {
-            //return new ThreadSafeBattleEyeServer(new BattlEyeServerProxy(new BattlEyeClient(credentials)
-            //{
-            //    ReconnectOnPacketLoss = true
-            //}), _log);
+            return new ThreadSafeBattleEyeServer(new BattlEyeServerProxy(new BattlEyeClient(credentials)
+            {
+                ReconnectOnPacketLoss = true
+            }), _log);
 
-            return new BattlEyeServerLogProxy(new MockBattleEyeServer(), _log);
+            //return new BattlEyeServerLogProxy(new MockBattleEyeServer(), _log);
         }
     }
 }
