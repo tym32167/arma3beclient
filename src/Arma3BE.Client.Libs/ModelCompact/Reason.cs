b16a5e526a12ba387fdbb2bc114048ccc92bef0b
Artem Muradov <tym32167@gmail.com>
2016-08-10 23:07:22 +0200

    #18 added kick/ban reasons

---
+++ src/Arma3BE.Client.Libs/ModelCompact/Reason.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Arma3BEClient.Libs.ModelCompact
+{
+    public class Reason
+    {
+        public string Text { get; set; }
+    }
+
+    public class BanTime
+    {
+        public string Title { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
