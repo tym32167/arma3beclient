7fc2c5cec3fa85bf91cdfcdd99a7f6f0ece9c6f0
Artem Muradov <tym32167@gmail.com>
2016-08-26 19:14:09 +0200

    #18 empty configuration

---
+++ src/Arma3BE.Client.Libs/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace Arma3BEClient.Libs.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Arma3BEClient.Libs.Context.Arma3BeClientContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Arma3BEClient.Libs.Context.Arma3BeClientContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
