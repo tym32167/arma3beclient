be09a218c454ebcc1922e6acfd8d01647e265f27
Artem Muradov <tym32167@gmail.com>
2016-09-01 00:16:22 +0200

    #18 #15 ban/kick reasons and ban time from DB

--- src/Arma3BE.Client.Libs/Migrations/Configuration.cs
+++ src/Arma3BE.Client.Libs/Migrations/Configuration.cs
@@ -1,18 +1,18 @@
+using System.Data.Entity.Migrations;
+using System.Linq;
+using Arma3BEClient.Libs.Context;
+using Arma3BEClient.Libs.ModelCompact;
+
 namespace Arma3BEClient.Libs.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<Arma3BEClient.Libs.Context.Arma3BeClientContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Arma3BeClientContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(Arma3BEClient.Libs.Context.Arma3BeClientContext context)
+        protected override void Seed(Arma3BeClientContext context)
         {
             //  This method will be called after migrating to the latest version.
 
@@ -26,6 +26,41 @@ namespace Arma3BEClient.Libs.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+
+            if (context.BanReasons.Any() == false)
+            {
+                context.BanReasons.AddOrUpdate(x => x.Text,
+                    new BanReason { Text = "Fill nickname" },
+                    new BanReason { Text = "Sabotage" },
+                    new BanReason { Text = "Teamkill" },
+                    new BanReason { Text = "Inadequate" },
+                    new BanReason { Text = "Troll" },
+                    new BanReason { Text = "Flud" }
+                    );
+            }
+
+            if (context.KickReasons.Any() == false)
+            {
+                context.KickReasons.AddOrUpdate(x => x.Text,
+                    new KickReason { Text = "Fill nickname" },
+                    new KickReason { Text = "Sabotage" },
+                    new KickReason { Text = "Teamkill" },
+                    new KickReason { Text = "Inadequate" },
+                    new KickReason { Text = "Troll" },
+                    new KickReason { Text = "Flud" }
+                    );
+            }
+
+            if (context.BanTimes.Any() == false)
+            {
+                context.BanTimes.AddOrUpdate(x => x.TimeInMinutes,
+                    new BanTime { Title = "Permanent", TimeInMinutes = 0 },
+                    new BanTime { Title = "Day", TimeInMinutes = 1440 },
+                    new BanTime { Title = "Week", TimeInMinutes = 10080 },
+                    new BanTime { Title = "Month", TimeInMinutes = 43200 }
+                    );
+            }
         }
     }
 }
