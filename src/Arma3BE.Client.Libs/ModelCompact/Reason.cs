a0a1e3b78993c47b65de911019cae32033cc1a3c
Artem Muradov <tym32167@gmail.com>
2016-08-31 21:37:48 +0200

    #18 migration-based mechanisms

--- src/Arma3BE.Client.Libs/ModelCompact/Reason.cs
+++ src/Arma3BE.Client.Libs/ModelCompact/Reason.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Arma3BEClient.Libs.ModelCompact
 {
-    public class Reason
+    public class BanReason
     {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class KickReason
+    {
+        [Key]
+        public int Id { get; set; }
         public string Text { get; set; }
     }
 
     public class BanTime
     {
+        [Key]
+        public int Id { get; set; }
         public string Title { get; set; }
-        public TimeSpan Time { get; set; }
+        public int TimeInMinutes { get; set; }
     }
 }
