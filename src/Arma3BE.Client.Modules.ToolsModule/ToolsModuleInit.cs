626e7f405ccda5b5a2a7c9720716737794a492cd
Artem Muradov <tym32167@gmail.com>
2016-10-12 23:20:11 +0200

    #46 export interface

--- src/Arma3BE.Client.Modules.ToolsModule/ToolsModuleInit.cs
+++ src/Arma3BE.Client.Modules.ToolsModule/ToolsModuleInit.cs
@@ -1,8 +1,11 @@
 ﻿using Arma3BE.Client.Infrastructure;
+using Arma3BE.Client.Modules.ToolsModule.ViewModels;
+using Arma3BE.Client.Modules.ToolsModule.Windows;
 using Microsoft.Practices.Unity;
 using Prism.Commands;
 using Prism.Modularity;
 using Prism.Regions;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Arma3BE.Client.Modules.ToolsModule
@@ -31,7 +34,13 @@ namespace Arma3BE.Client.Modules.ToolsModule
                 Header = "Export players",
                 Command = new DelegateCommand(() =>
                 {
-                    _container.Resolve<Exporter>().Export();
+                    var wnd = _container.Resolve<ExportWindow>();
+                    var vm = _container.Resolve<ExportViewModel>();
+
+                    wnd.Owner = Application.Current.MainWindow;
+                    wnd.DataContext = vm;
+
+                    wnd.ShowDialog();
                 })
             };
         }
