3dfd511013b6e18bf4a06b85abf68363d9f7d6ff
Artem Muradov <tym32167@gmail.com>
2017-06-21 19:56:54 +0200

    #79

--- src/Arma3BE.Client.Modules.NetModule/IpService.cs
+++ src/Arma3BE.Client.Modules.NetModule/IpService.cs
@@ -1,15 +1,33 @@
 ﻿using Arma3BE.Client.Infrastructure;
+using Arma3BEClient.Common.Core;
+using Arma3BEClient.Common.Logging;
 using MaxMind.GeoIP2;
+using MaxMind.GeoIP2.Responses;
 using System;
 using System.Configuration;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Arma3BE.Client.Modules.NetModule
 {
-    public class IpService : IIpService
+    public class IpService : DisposeObject, IIpService
     {
+        private DatabaseReader reader;
+        private static readonly ILog Log = LogFactory.Create(new StackTrace().GetFrame(0).GetMethod().DeclaringType);
+
+        public IpService()
+        {
+            reader = new DatabaseReader(@"IPDatabase\GeoLite2-City.mmdb");
+        }
+
+        protected override void DisposeManagedResources()
+        {
+            base.DisposeManagedResources();
+            reader.Dispose();
+        }
+
         public string GetIpAddress(string host)
         {
             IPAddress ip;
@@ -46,16 +64,17 @@ namespace Arma3BE.Client.Modules.NetModule
 
             try
             {
-                using (var reader = new DatabaseReader(@"IPDatabase\GeoLite2-City.mmdb"))
+                CityResponse city;
+                if (reader.TryCity(ip, out city))
                 {
-                    var city = reader.City(ip);
-
                     var result = new GeoInfo(city.Country.Name, city.City.Name);
                     return result;
                 }
+                return default(GeoInfo);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Log.Error(ex);
                 return default(GeoInfo);
             }
         }
