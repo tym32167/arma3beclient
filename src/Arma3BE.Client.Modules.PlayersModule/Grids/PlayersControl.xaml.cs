5b266a3e5bcaa560a2db4acdc74c9fc4c8c84196
Artem Muradov <tym32167@gmail.com>
2017-06-25 16:16:57 +0200

    #82

--- src/Arma3BE.Client.Modules.PlayersModule/Grids/PlayersControl.xaml.cs
+++ src/Arma3BE.Client.Modules.PlayersModule/Grids/PlayersControl.xaml.cs
@@ -1,8 +1,12 @@
-﻿using Arma3BE.Client.Infrastructure.Extensions;
+﻿using Arma3BE.Client.Infrastructure.Events;
+using Arma3BE.Client.Infrastructure.Events.Models;
+using Arma3BE.Client.Infrastructure.Extensions;
 using Arma3BE.Client.Modules.PlayersModule.Models;
+using Prism.Events;
 using Prism.Regions;
 using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Arma3BE.Client.Modules.PlayersModule.Grids
 {
@@ -13,8 +17,11 @@ namespace Arma3BE.Client.Modules.PlayersModule.Grids
     // ReSharper disable once RedundantExtendsListEntry
     public partial class PlayersControl : UserControl
     {
-        public PlayersControl()
+        private readonly IEventAggregator _eventAggregator;
+
+        public PlayersControl(IEventAggregator eventAggregator)
         {
+            _eventAggregator = eventAggregator;
             InitializeComponent();
 
             dg.LoadState<PlayerView>(GetType().FullName);
@@ -25,6 +32,15 @@ namespace Arma3BE.Client.Modules.PlayersModule.Grids
             {
                 menu.Items.Remove(menuItem);
                 dg.ContextMenu?.Items.Add(menuItem);
+            }
+        }
+
+        private void Dg_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var player = dg.SelectedItem as PlayerView;
+            if (player != null)
+            {
+                _eventAggregator.GetEvent<ShowUserInfoEvent>().Publish(new ShowUserModel(player.Guid));
             }
         }
     }
