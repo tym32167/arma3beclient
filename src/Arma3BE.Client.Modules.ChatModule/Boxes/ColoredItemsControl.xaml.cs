8868a82a90b257e2a4cf8ab1afe7a797452f6984
Artem Muradov <tym32167@gmail.com>
2017-10-22 21:00:26 +0200

    #26 virt players log

---
+++ src/Arma3BE.Client.Modules.ChatModule/Boxes/ColoredItemsControl.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Arma3BE.Client.Infrastructure.Events;
+using Arma3BE.Client.Infrastructure.Events.Models;
+using Arma3BE.Client.Infrastructure.Extensions;
+using Arma3BE.Server.Models;
+using Microsoft.Practices.ServiceLocation;
+using Prism.Commands;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Xceed.Wpf.AvalonDock.Controls;
+
+namespace Arma3BE.Client.Modules.ChatModule.Boxes
+{
+    /// <summary>
+    /// Interaction logic for ColoredItemsControl.xaml
+    /// </summary>
+    public partial class ColoredItemsControl : UserControl, IColoredTextControl
+    {
+        public ColoredItemsControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            ShowPlayerCommand = new DelegateCommand<PlayerMessage>(p =>
+            {
+                var aggregator = ServiceLocator.Current.TryResolve<IEventAggregator>();
+                aggregator.GetEvent<ShowUserInfoEvent>().Publish(new ShowUserModel(p.PlayerId));
+            });
+        }
+
+        public ICommand ShowPlayerCommand { get; set; }
+
+        public void AppendPlayerText(KeyValuePair<string, string> player, bool isIn)
+        {
+            Messages.Add(new PlayerMessage { PlayerId = player.Key, PlayerName = player.Value, isIn = isIn, DateTime = DateTime.UtcNow.UtcToLocalFromSettings() });
+            ScrollIfNeeded();
+        }
+
+        public void AppendText(ChatMessage message, string servername = null)
+        {
+            Messages.Add(new FullChatMessage { Message = message, Server = servername });
+            ScrollIfNeeded();
+        }
+
+        public void ClearAll()
+        {
+            Messages.Clear();
+        }
+
+        void ScrollIfNeeded()
+        {
+            if (!IsAutoScroll) return;
+
+            var sv = lv.FindVisualChildren<ScrollViewer>().FirstOrDefault();
+            sv?.ScrollToEnd();
+        }
+
+        public ObservableCollection<object> Messages { get; set; } = new ObservableCollection<object>();
+
+        public static readonly DependencyProperty IsAutoScrollProperty =
+            DependencyProperty.Register("IsAutoScroll", typeof(Boolean), typeof(ColoredItemsControl),
+                new FrameworkPropertyMetadata(false));
+
+
+        public bool IsAutoScroll
+        {
+            get { return (bool)GetValue(IsAutoScrollProperty); }
+            set { SetValue(IsAutoScrollProperty, value); }
+        }
+    }
+
+    public class PlayerMessage
+    {
+        public string PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public bool isIn { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+
+
+    public class FullChatMessage
+    {
+        public string Server { get; set; }
+        public ChatMessage Message { get; set; }
+    }
+
+
+
+}
---
+++ src/Arma3BE.Client.Modules.ChatModule/Boxes/Converters/PlayerMessageToColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Arma3BE.Client.Modules.ChatModule.Boxes.Converters
+{
+    public class PlayerMessageToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((value as PlayerMessage)?.isIn == true) return Brushes.Green;
+            else return Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
