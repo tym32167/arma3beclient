c112b84fd4af44b466c37a8591a5061725b1b660
Artem Muradov <tym32167@gmail.com>
2018-07-29 09:29:20 +0200

    - switch to next release 0.9

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyVersion("0.9.*")]
 [assembly: System.Resources.NeutralResourcesLanguage("en")]
